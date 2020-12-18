cabc35bd5df1da0737ff28404a26a543e310d126
ielm <leoni@rpi.edu>
2020-12-18 13:48:20 -0500

    v0.1.0 init

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="OntoGen",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "OntoGraph",
+        "leialexicon",
+        "leiaontology",
+        "Flask",
+        "Flask-Cors",
+        "Flask-SocketIO",
+        "requests",
+        "nlglib",
+    ],
+    author="Ivan Leon",
+    author_email="leoni@rpi.edu",
+    description="LEIA natural language generation service",
+    keywords="NLG",
+    project_urls={
+        "Documentation": "https://app.nuclino.com/LEIA/OntoGen/",
+        "Source Code": "https://bitbucket.org/ielm/OntoGen/src/master/",
+    },
+)
