8cb7ff08811e4d64c63e3c7ceca54ff530714a66
ielm <leoni@rpi.edu>
2021-02-25 12:56:06 -0500

    format style to black

--- ontogen/utils/cxparser.py
+++ ontogen/utils/cxparser.py
@@ -3,15 +3,18 @@ import sys
 from typing import Any, List, OrderedDict
 from pprint import pprint
 
-EXCLUDED_SYMBOLS = ['-', '$', '*', '^', '"', '?', '.', '!']
-LITERAL_KEYS = ['DEF', 'CAT', 'TMR-HEAD', 'EX', 'COMMENTS', 'EXAMPLE-BINDINGS']
+EXCLUDED_SYMBOLS = ["-", "$", "*", "^", '"', "?", ".", "!"]
+LITERAL_KEYS = ["DEF", "CAT", "TMR-HEAD", "EX", "COMMENTS", "EXAMPLE-BINDINGS"]
+
 
 def normalize_str(string: str) -> List[str]:
     str_norm = []
     last_c = None
     for c in string:  # For each character in the input string
         if c.isalnum() or c in EXCLUDED_SYMBOLS:  # If char is alphanumeric
-            if last_c.isalnum() or last_c in EXCLUDED_SYMBOLS:  # If last char is alphanumeric
+            if (
+                last_c.isalnum() or last_c in EXCLUDED_SYMBOLS
+            ):  # If last char is alphanumeric
                 str_norm[-1] += c  # Append char to last str in str_norm list
             else:  # If char is not alphanumeric
                 str_norm.append(c)  # append char to str_norm list
@@ -19,6 +22,7 @@ def normalize_str(string: str) -> List[str]:
             str_norm.append(c)  # Append char to str_norm list
         last_c = c  # Set last char to current char
     return str_norm
+
 
 # Generate abstract syntax tree from normalized input.
 def get_ast(input_norm: List[str]) -> List[Any]:
@@ -31,30 +35,31 @@ def get_ast(input_norm: List[str]) -> List[Any]:
         # If the element is an open parenthesis, find matching parenthesis and make
         # recursive call for content in between. Add the result as an element to the
         # current list
-        if symbol == '(': 
+        if symbol == "(":
             list_content = []
-            match_ctr = 1 # If 0, parenthesis has been matched
+            match_ctr = 1  # If 0, parenthesis has been matched
             while match_ctr != 0:
                 i += 1
                 if i >= len(input_norm):
                     raise ValueError("Invalid input: Unmatched open parenthesis.")
                 symbol = input_norm[i]
-                if symbol == '(':
+                if symbol == "(":
                     match_ctr += 1
-                elif symbol == ')':
+                elif symbol == ")":
                     match_ctr -= 1
                 if match_ctr != 0:
-                    list_content.append(symbol)             
+                    list_content.append(symbol)
             ast.append(get_ast(list_content))
-        elif symbol == ')': # Element should have been closed 
-                raise ValueError("Invalid input: Unmatched close parenthesis.")
-        else: # If the element is an atom, just add it to the current list
+        elif symbol == ")":  # Element should have been closed
+            raise ValueError("Invalid input: Unmatched close parenthesis.")
+        else:  # If the element is an atom, just add it to the current list
             try:
                 ast.append(int(symbol))
             except ValueError:
                 ast.append(symbol)
         i += 1
     return ast
+
 
 def build_cx(cx: List) -> OrderedDict:
     _id = cx.pop(0)
@@ -75,19 +80,19 @@ def build_cx(cx: List) -> OrderedDict:
     for slot in _slots:
         # print(slot)
         if slot[0] in LITERAL_KEYS:
-            if slot[0] == 'EXAMPLE-BINDINGS':
-                _str = ' '.join(slot[1]).strip()
+            if slot[0] == "EXAMPLE-BINDINGS":
+                _str = " ".join(slot[1]).strip()
                 print(f"{slot[0]}\t{_str}")
-                cx[slot[0]] =  _str
+                cx[slot[0]] = _str
             else:
-                _str = ' '.join(slot[1:]).strip('"').strip()
+                _str = " ".join(slot[1:]).strip('"').strip()
                 print(f"{slot[ 0]}\t{_str}")
                 cx[slot[0]] = _str
         # else:
-            # print(f"\n{slot[0]}\t{slot[1:]}")
-            # print(f"\n{slot[0]}")
-            # print(f"\n\t")
-            # build_slot(slot[1])
+        # print(f"\n{slot[0]}\t{slot[1:]}")
+        # print(f"\n{slot[0]}")
+        # print(f"\n\t")
+        # build_slot(slot[1])
 
         elif slot[0] == "SYN-STRUC":
             print(f"{slot[0]}")
@@ -98,7 +103,7 @@ def build_cx(cx: List) -> OrderedDict:
             print(f"{slot[0]}")
             pprint(slot[1:])
 
-        # if the first element 
+        # if the first element
     return cx
 
 
@@ -109,23 +114,17 @@ def parse_lisp(input_str) -> List[dict]:
     cxs = []
 
     for c in lisp_cxs:
-    	# parse the construction into a python-readable dictionary
+        # parse the construction into a python-readable dictionary
         cx = build_cx(c)
         cxs.append(cx)
 
     return cxs
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     data = ""
-    with open('../knowledge/constructions/speech-acts-lexicon.lisp', 'r') as file:
-        data = file.read() #.replace('\n', '')
+    with open("../knowledge/constructions/speech-acts-lexicon.lisp", "r") as file:
+        data = file.read()  # .replace('\n', '')
 
     a = parse_lisp(data)
-
-
-
-
-
-
-
