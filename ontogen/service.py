300efd9e8ce830832af49579c91cc6a29f204e8f
ielm <leoni@rpi.edu>
2021-02-25 12:54:50 -0500

    (wip) format style to black

--- ontogen/service.py
+++ ontogen/service.py
@@ -21,23 +21,41 @@ def api_generate():
     log = "log" in request.form and request.form["log"].lower() == "true"
     dc = "dc" in request.form and request.form["dc"].lower() == "true"
     dmp = "dmp" in request.form and request.form["dmp"].lower() == "true"
-    return_one_result = "oneresult" in request.form and request.form["oneresult"].lower() == "true"
+    return_one_result = (
+        "oneresult" in request.form and request.form["oneresult"].lower() == "true"
+    )
     is_robot = "isrobot" in request.form and request.form["isrobot"].lower() == "true"
 
     rargs = "rargs" in request.form and request.form["rargs"].lower() == "true"
-    rargs_root_agent = request.form["rargs-root-agent"] if "rargs-root-agent" in request.form else ""
-    rargs_root_beneficiary = request.form["rargs-root-beneficiary"] if "rargs-root-beneficiary" in request.form else ""
-    rargs_frame_prefix = request.form["rargs-prefix"] if "rargs-prefix" in request.form else ""
-    rargs_frame_delim = request.form["rargs-delim"] if "rargs-delim" in request.form else ""
-    rargs_concept_prefix = request.form["ont-space"] if "ont-space" in request.form else ""
+    rargs_root_agent = (
+        request.form["rargs-root-agent"] if "rargs-root-agent" in request.form else ""
+    )
+    rargs_root_beneficiary = (
+        request.form["rargs-root-beneficiary"]
+        if "rargs-root-beneficiary" in request.form
+        else ""
+    )
+    rargs_frame_prefix = (
+        request.form["rargs-prefix"] if "rargs-prefix" in request.form else ""
+    )
+    rargs_frame_delim = (
+        request.form["rargs-delim"] if "rargs-delim" in request.form else ""
+    )
+    rargs_concept_prefix = (
+        request.form["ont-space"] if "ont-space" in request.form else ""
+    )
 
-    robot_args = {
-        "root-agent": rargs_root_agent,
-        "root-beneficiary": rargs_root_beneficiary,
-        "frame-prefix": rargs_frame_prefix,
-        "frame-delim": rargs_frame_delim,
-        "concept-prefix": "@" + rargs_concept_prefix + ".",
-    } if rargs else {}
+    robot_args = (
+        {
+            "root-agent": rargs_root_agent,
+            "root-beneficiary": rargs_root_beneficiary,
+            "frame-prefix": rargs_frame_prefix,
+            "frame-delim": rargs_frame_delim,
+            "concept-prefix": "@" + rargs_concept_prefix + ".",
+        }
+        if rargs
+        else {}
+    )
 
     try:
         tmr = request.args["otmr"]
