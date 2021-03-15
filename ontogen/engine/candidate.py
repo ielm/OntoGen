47055a0b3051dc195cf0730793eec0e0781baea7
ielm <leoni@rpi.edu>
2021-03-14 22:48:20 -0400

    (wip) add to_dict and to_str to candidates

--- ontogen/engine/candidate.py
+++ ontogen/engine/candidate.py
@@ -1,20 +1,34 @@
-from ontogen.config import OntoGenConfig
 from ontogen.knowledge.lexicon import Sense
+from typing import Tuple
+
+import uuid
 
 
 class RealizationCandidate:
-    """
-    Whereas OntoSem deals with TMR candidates, OntoGen deals with realization
-    candidates. This means that the primary objective is to linearize the syn and
-    sem maps of the tmrframe construction candidates.
-    """
-
-    def __init__(self, *senses: ConstructionCandidate):
+    def __init__(self, senses: Tuple["ConstructionCandidate"]):
         self.id = str(uuid.uuid4())
         self.constructions = senses
         self.constraints = []
         self.scores = []
         self.score = 0.0
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "constructions": self.constructions.to_dict,
+            "constraints": self.constraints,
+            "scores": self.scores,
+            "score": self.score,
+        }
+
+    def to_str(self):
+        s = f"\nRealization Candidate #{self.id}"
+        for cx in self.constructions:
+            s += f"\n{cx.to_str()}"
+        return s
+
+    def __repr__(self):
+        return self.to_str()
 
 
 class ConstructionCandidate:
@@ -27,18 +41,21 @@ class ConstructionCandidate:
     def to_dict(self) -> dict:
         return {
             "id": self.id,
-            "pos": self.pos,
+            "pos": self.sense.pos,
             "tmr_anchor": self.tmr_anchor,
             "synstruc": self.sense.synstruc.to_dict(),
             "semstruc": self.sense.semstruc.to_dict(),
         }
 
     def to_str(self, indent: int = 4) -> str:
-        s = f"\n{' '*indent}id:     \t{self.id}\n"
-        s += f"{' '*indent}pos:     \t{self.sense.pos}\n"
-        s += f"{' '*indent}tmr_anchor: \t{self.tmr_anchor}\n"
-        s += f"{' '*indent}synstruc:\t{self.sense.synstruc.to_str()}\n"
-        s += f"{' '*indent}semstruc:\t{self.sense.semstruc.to_str()}"
+        s = f"\n{' '*(indent)}v{'-'*10}\n"
+        s += f"{' '*(indent)}  id:         \t{self.id}\n"
+        s += f"{' '*(indent)}\tpos:        \t{self.sense.pos}\n"
+        s += f"{' '*(indent)}\ttmr_anchor: \t{self.tmr_anchor}\n"
+        s += f"{' '*(indent)}\tsynstruc:   \t{self.sense.synstruc.to_str(indent*6)}\n"
+        s += f"{' '*(indent)}\tsemstruc:   \t{self.sense.semstruc.to_str(indent*6)}\n"
+        s += f"{' '*(indent)}^{'-'*10}"
+
         return s
 
     def __repr__(self):
