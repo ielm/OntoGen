6d37ee5e2d102553189dcec86bf06999803988fa
ielm <leoni@rpi.edu>
2021-03-14 21:08:16 -0400

    (wip) preprocess and run semantic compiler

--- ontogen/runner.py
+++ ontogen/runner.py
@@ -1,82 +1,91 @@
-from ontogen.engine.analysis import Analysis
-from ontogen.engine.construction import generate_cx_combinations, CombinationBuilder
+from ontogen.engine.semantics.compiler import SemanticCompiler
+from ontogen.engine.preprocessor import Preprocessor
+from ontogen.engine.report import Report
 from ontogen.engine.otmr import oTMR
+from ontogen.knowledge.ontology import Ontology
+from ontogen.knowledge.lexicon import Lexicon
 from ontogen.config import OntoGenConfig
 
-
-from typing import Union, List
+from typing import Union
 
 import json
 import sys
+import ast
 
 
 class OntoGenRunner:
-
-    def __init__(self, config: OntoGenConfig):
-        self.config = config
+    def __init__(
+        self, config: OntoGenConfig, ontology: Ontology = None, lexicon: Lexicon = None
+    ):
+        self.config = config if config is not None else OntoGenConfig()
+        self.ontology = ontology if ontology is not None else self.config.ontology()
+        self.lexicon = lexicon if lexicon is not None else self.config.lexicon()
 
     def run_from_file(self, file: str):
         f = open(file, "r")
-        tmrs = json.load(f)
-        print("\n Loaded", len(tmrs), "TMRs.\n")
+        otmrs = json.load(f)
+        print("\n Loaded", len(otmrs), "oTMRs.\n")
         f.close()
 
-        return [self.run(t) for t in tmrs]
-
-
+        return [self.run(t) for t in otmrs]
 
     def run(self, otmr: Union[oTMR, dict]):
-        # Only ever runs one sentence at a time
-        log = OntoGenLogger(self.config)
+        if isinstance(otmr, dict):
+            otmr = oTMR.instance(self.config, otmr)
+            otmr.set_ontology(self.ontology)
+            otmr.set_lexicon(self.lexicon)
 
-        # if not isinstance(otmr, oTMR):
-            # otmr = oTMR.instance_from_dict(tmr)
+        report = Report(self.config, otmr)
 
-        # Preprocess
-        # I was thinking that here is where situational MPs can be run
-        # pp = Preprocessor(self.config).run(otmr)
-
-        # Generate Candidate Construction Combinations
-        # combinations = CombinationBuilder(self.config).run(pp)
-        # combinations = generate_cx_combinations(otmr)
-
-        # Prune Candidate Combinations
+        # Preprocess - resolve any MPs and reference frames
+        pp = Preprocessor(self.config).run(otmr)  # r: preprocessed otmr
+        _ = SemanticCompiler(self.config, self.ontology, self.lexicon).run(pp)
 
         # Generate Sentences for Viable Candidates
 
         # Select the Best Candidate Utterance
-        pass
+
+        return Report
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     arguments = sys.argv
 
-    if len(arguments) < 2 or (len(arguments) == 2 and arguments[1].startswith("config=")):
-        print("Correct usage: runner.py \"Input text here.\"")
-        print("Optional config parameter: runner.py config=ontosem.yml \"Input text here.\"")
+    if len(arguments) < 2 or (
+        len(arguments) == 2 and arguments[1].startswith("config=")
+    ):
+        print("Correct usage: runner.py tmr_index")
+        print("Optional config parameter: runner.py config=ontogen.yml tmr_index")
 
     arguments = arguments[1:]
 
     config = OntoGenConfig()
     if arguments[0].startswith("config="):
-        config_file = arguments[0].replace("config=", "")
+        config_file = arguments[1].replace("config=", "")
         config = OntoGenConfig().from_file(config_file)
         arguments = arguments[1:]
 
-    sentence = arguments[0]
+    with open("tests/resources/sample-TMRs.txt", "r") as file:
+        data = file.read()
+        tmrs = ast.literal_eval(data)
+
+    tmr_index = int(arguments[0])
+    temp = tmrs[tmr_index]["results"][0]["TMR"]
+    tmr = {"sentence": tmrs[tmr_index]["sentence"], "tmr": {}}
+
+    for key in temp.keys():
+        if key.lower() == key:
+            # print(f"---skipping--- {key}: \t {temp[key]}")
+            continue  # Skip non-frame elements
+        tmr["tmr"][key] = temp[key]
+
+    # == DEV PRINTS == #
+    # print(f"TMR FOR: {tmr['sentence']}")
+    # pprint(tmr["tmr"])
+    # print(f"\n{'-'*80}\n")
 
     config.load_knowledge()
-    runner = OntoGenmRunner(config)
-    results = runner.run([sentence])
-
-    for frame in results.sentences[0].semantics[0].basic_tmr.frames.values():
-        print(frame.frame_id())
-        for p, fillers in frame.properties.items():
-            print("--%s = %s" % (p, ",".join(map(lambda f: str(f), fillers))))
-    print("----")
-    print("SCORE: %f" % results.sentences[0].semantics[0].score)
-    print("SCORING:")
-    for score in results.sentences[0].semantics[0].scores:
-        print("-- %s" % score)
-
-    print(json.dumps(results.to_dict(), indent=2))
+    runner = OntoGenRunner(
+        config,
+    )
+    results = runner.run(tmr["tmr"])
